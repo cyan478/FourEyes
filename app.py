0ef6f8668ef54819d2b46039bfe5309bbd4eaeb7
cyan478 <cyan2@stuy.edu>
2017-10-07 16:59:41 -0400

    visual recog works

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-import sys
 import json
+
+import sys
+import os
+
 from flask import Flask, render_template, request
-from watson_developer_cloud import ToneAnalyzerV3
+from watson_developer_cloud import VisualRecognitionV3
+
 
 app = Flask(__name__)
 
@@ -17,17 +21,19 @@ def post_image():
 # /visualrec -> "visual recommendations"
 @app.route('/visualrec', methods=["GET"]) #receives image upload and produces map from foursquare api
 def render_results():
-	u = "d6094380-8c1e-4522-9f9d-97aeadb47356"
-	p = "vSAcDfdNZr1r"
-	v = "2016-05-19"
-	text = request.values['tone'] #from dictionary, textarea name = tone, pull out input words
-	t = ToneAnalyzerV3(username=u, password=p, version=v)
-	d = t.tone(text)
+
+	visual_recognition = VisualRecognitionV3('2016-05-20', api_key='c5666e0f4f55241567cee1f69c0652e7e86d8ffe')
+	imgObject = visual_recognition.classify(images_file=open("sample-pictures/swing.jpg", "rb")) #take in image file
+	#print json.dumps(imgObject) 
+	objectName = imgObject['images'][0]['classifiers'][0]['classes'][0]['class'] #image's class / name
+	return objectName 
+
+
+
+
 	
-	from IPython import embed; embed() #COOL DEBUGGING TECHNIQUE
-	return render_template("tone.html")
-
-
+	#from IPython import embed; embed() #COOL DEBUGGING TECHNIQUE
+	#return render_template("tone.html")
 
 
 
@@ -38,8 +44,8 @@ app.run(host="0.0.0.0", port=3333, debug=True)
 '''
 
 {
-  "url": "https://gateway.watsonplatform.net/tone-analyzer/api",
-  "username": "d6094380-8c1e-4522-9f9d-97aeadb47356",
-  "password": "vSAcDfdNZr1r"
+  "url": "https://gateway-a.watsonplatform.net/visual-recognition/api",
+  "note": "It may take up to 5 minutes for this key to become active",
+  "api_key": "c5666e0f4f55241567cee1f69c0652e7e86d8ffe"
 }
 '''
