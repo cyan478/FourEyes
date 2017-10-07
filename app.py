be2555a3c8dff603b4e365dffd75b773e9305ca4
Kevin Zhang <kevinzhang3702@gmail.com>
2017-10-07 16:14:06 -0400

    wew

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-import sys
+import sys, os
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 from watson_developer_cloud import ToneAnalyzerV3
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'images'
+ALLOWED_EXTENSIONS = [".jpg",".jpeg",".png"]
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route('/', methods=["GET"])
 def home():
@@ -28,6 +34,56 @@ def post_tone():
 	
 	from IPython import embed; embed() #COOL DEBUGGING TECHNIQUE
 	return render_template("tone.html")
+
+
+
+# File Upload Stuff
+
+# Checks if file is one of the allowed extensions
+def allowed_file(filename):
+        return '.' in filename and \
+                filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/upload", methods=["GET"])
+def upload():
+        return render_template("upload.html")
+
+@app.route("/upload", methods=["POST"])
+def upload_file():
+
+        #check if there is a file
+        if 'file' not in request.files:
+                flash("File Not Found")
+                return redirect(url_for('upload')) # Refinement Suggestion: Send JSON to Client instead
+
+        #retrieve file
+        file = request.files['file']
+
+        #check that it is a file
+        if not file:
+                flash("Not A File")
+                return redirect(url_for('upload')) # Refinement Suggestion: Send JSON to Client instead
+        
+        #check that the file has a name 
+        if file.filename == '':
+                flash("No Selectec File")
+                return redirect(url_for('upload')) # Refinement Suggestion: Send JSON to Client instead
+        
+        
+        #check that file is allowed
+        if not allowed_file( file.filename ) :
+                flash("File Extension not Allowed")
+                return redirect(url_for('upload')) # Refinement Suggestion: Send JSON to Client instead
+
+        #Upload File and Store it
+        filename = secure_filename( file.filename ) 
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('success_upload'))
+
+@app.route("/success", methods=["GET"])
+def success_upload():
+        return render_template("success.html")
 
 
 app.run(host="0.0.0.0", port=3333, debug=True)
